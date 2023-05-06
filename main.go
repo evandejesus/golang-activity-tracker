dc962739e48159fa1ce9b2beddf4ddb4514ab318
Evan de Jesus <evanjdejesus@gmail.com>
2023-05-06 14:09:56 -0600

    initial commit

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/", handlePost).Methods("POST")
+	r.HandleFunc("/", handleGet).Methods("GET")
+	srv := &http.Server{Addr: ":8080", Handler: r}
+	fmt.Println("fuck you")
+	srv.ListenAndServe()
+}
+
+func handleGet(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w, "get\n")
+}
+
+func handlePost(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w, "post\n")
+}
